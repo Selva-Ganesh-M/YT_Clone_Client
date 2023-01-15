dcfcfdadf89e5743ae226864770e593334c5b7cd
Selva Ganesh M <selvadev2k@gmail.com>
2023-01-15 18:10:27 +0530

    static frontend final commit

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -63,30 +63,30 @@ const Link = styled.span`
 `;
 
 const SignIn = () => {
-    return (
-        <Container>
-            <Wrapper>
-                <Title>Sign in</Title>
-                <SubTitle>to continue to LamaTube</SubTitle>
-                <Input placeholder="username" />
-                <Input type="password" placeholder="password" />
-                <Button>Sign in</Button>
-                <Title>or</Title>
-                <Input placeholder="username" />
-                <Input placeholder="email" />
-                <Input type="password" placeholder="password" />
-                <Button>Sign up</Button>
-            </Wrapper>
-            <More>
-                English(USA)
-                <Links>
-                    <Link>Help</Link>
-                    <Link>Privacy</Link>
-                    <Link>Terms</Link>
-                </Links>
-            </More>
-        </Container>
-    );
+  return (
+    <Container>
+      <Wrapper>
+        <Title>Sign in</Title>
+        <SubTitle>to continue to YT Clone</SubTitle>
+        <Input placeholder="username" />
+        <Input type="password" placeholder="password" />
+        <Button>Sign in</Button>
+        <Title>or</Title>
+        <Input placeholder="username" />
+        <Input placeholder="email" />
+        <Input type="password" placeholder="password" />
+        <Button>Sign up</Button>
+      </Wrapper>
+      <More>
+        English(USA)
+        <Links>
+          <Link>Help</Link>
+          <Link>Privacy</Link>
+          <Link>Terms</Link>
+        </Links>
+      </More>
+    </Container>
+  );
 };
 
 export default SignIn;
